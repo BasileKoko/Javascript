56dbc8f74fdc4cd58a7a90799bc933ea19bfcf9b
Basile Koko <basile_koko@hotmail.com>
2017-02-02 09:26:45 +0000

    adding JQuery

--- thermostat/index.js
+++ thermostat/index.js
@@ -1,12 +1,39 @@
 $(document).ready(function(){
+  var thermostat = new ThermoStat();
 
 
+  $('#up').on('click', function(){
+    thermostat.up();
+    updateTemp();
+  })
 
 
+  $('#savemode_off').on('click', function(){
+    thermostat.isPowerSaving == false;
+    $('#power_save').text('Power Save: OFF');
+
+  })
+
+  $('#savemode_on').on('click', function(){
+     thermostat.isPowerSaving == true;
+     $('#power_save').text('Power Save: ON');
+  })
+
+  $('#down').on('click', function(){
+      thermostat.down();
+      updateTemp();
+  })
+
+  $('#reset').on('click', function(){
+    thermostat.reset();
+    updateTemp();
+
+  })
+
+  function updateTemp(){
+    $('#current_temp').text('Current Temperature: ' + thermostat.temperature);
+    $('#usage').text('Usage: ' + thermostat.usage());
+  }
 
 
-
-
-
-  
 })
